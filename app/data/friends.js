512213332dd0e5277825427749d33b5e5d5d89e6
arash <arash.teimourpoor@gmail.com>
2017-11-11 21:29:28 -0500

    final push

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+
+var friends = [
+    {
+        "name": "Paul",
+        "photo": "https://pbs.twimg.com/profile_images/744845474990014464/PlNkgS_b.jpg",
+        "scores":[2, 2, 1, 1, 3, 1, 1, 4, 1, 2]
+
+    },
+
+    {
+        "name": "Dani",
+        "photo": "https://media.theplayerstribune.com/uploads/dani-alves-avatar.png",
+        "scores":[2, 2, 3, 3, 2, 1, 4, 3, 3, 5]
+    },
+
+    {
+        "name": "Rachel",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/0/0a/Rachel_Green_Rachel_haircut.jpg",
+        "scores":[2, 2, 3, 2, 2, 3, 3, 1, 3, 4]
+    },
+
+    {
+        "name": "Jade",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQElOjFM12b23x61jjk5NZb4S-tA8941ehPV3sb6QyYf7EnNM3Q",
+        "scores":[2, 2, 3, 3, 4, 1, 1, 2, 3, 5]
+    }
+
+];
+
+module.exports = friends;
